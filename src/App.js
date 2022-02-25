83cd5d74606cc0f813ead85604522644338e864b
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-24 17:44:11 -0700

    Import Header component and DataGrid in App. header and data table render properly on page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-// import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
 import data from './Data/data';
+import DataGrid from 'react-data-grid';
 import { generateColumns } from './Data/data-utils';
-
-// import DataGrid from 'react-data-grid';
 // import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
-
 function App() {
 
   return (
     <div className="App">
       <div className="App-header">
-        HELLO      
+        <Header />
       </div>
       <div>
-        {/* <DataGrid rows={data}/> */}
+        <DataGrid 
+          columns={generateColumns(data)}
+          rows={data}/>
       </div>
     </div>
   );
